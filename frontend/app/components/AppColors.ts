63ac30d5b8bd38cfb3143adb14d34bb0351ee78e
Clément <clementproze@gmail.com>
2025-10-07 11:11:27 -0400

    new colors

--- frontend/app/components/AppColors.ts
+++ frontend/app/components/AppColors.ts
@@ -3,17 +3,20 @@ export const AppColors = {
   // Backgrounds
   backgroundDefault: '#FFFFFF',
   backgroundDimmer: '#F4F4F4',
+  backgroundDimmest: '#E5E5E5',
 
   // Foregrounds (Text)
   foregroundDefault: '#1F1F1F',
   foregroundDimmer: '#ABABAB',
 
   // Accent
-  accentDefault: '#FFADAD',
+  accentDefault: '#1B1B1B',
+  accentDimmer: '#505050',
 
   // Negative (Error/Destructive)
   negativeDefault: '#DB0500',
-  negativeDimmer: '#FDEFEF',
+  negativeDimmer: '#FAD9D9',
+  negativeDimmest: '#FDEFEF',
 } as const;
 
 // Type for color keys
