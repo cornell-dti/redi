ddb60d13ab4b2696302621c8310a05e01ae42ad4
Juliet Crane <jlc565@cornell.edu>
2025-11-09 12:19:53 -0500

    Update _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -270,7 +270,7 @@ function RootNavigator() {
   return (
     <>
       <Stack>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen
           name="home"
