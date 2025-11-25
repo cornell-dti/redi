7c12c17ea89e14ca818cceb3928dba6d43aee475
Juliet Crane <jlc565@cornell.edu>
2025-11-25 16:25:07 -0500

    remove google

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -29,7 +29,6 @@ import {
 } from './api/authService';
 import { getCurrentUserProfile } from './api/profileApi';
 import { AppColors } from './components/AppColors';
-import GoogleIcon from './components/icons/GoogleIcon';
 import OnboardingVideo from './components/onboarding/OnboardingVideo';
 import AppInput from './components/ui/AppInput';
 import AppText from './components/ui/AppText';
@@ -384,19 +383,19 @@ export default function HomePage() {
           transform: [{ translateX: buttonsSlideAnim }],
         }}
       >
-        <Button
+        {/* <Button
           title="Continue with Google"
           onPress={handleGoogleSignIn}
           variant="primary"
           fullWidth
           disabled={loading}
           iconLeft={GoogleIcon}
-        />
+        /> */}
 
         <Button
-          title="Sign in with Email Link"
+          title="Sign in / Sign up with Email"
           onPress={() => handleModeChange('passwordless')}
-          variant="secondary"
+          variant="primary"
           fullWidth
         />
 
