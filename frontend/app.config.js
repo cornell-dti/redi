1c905f689982a12345cb8ecad4597351bd8b333c
Juliet Crane <jlc565@cornell.edu>
2025-12-29 10:54:04 -0500

    Update app.config.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: 'redi',
     slug: 'redi',
-    version: '1.0.0',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'redi',
