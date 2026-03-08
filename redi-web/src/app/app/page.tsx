636b9752fc0a4dd73ae01ef029900ccf43be8ade
Abrar Amin <abraramin.school@gmail.com>
2026-03-07 19:34:21 -0500

    deleted useless console.log lol

--- redi-web/src/app/app/page.tsx
+++ redi-web/src/app/app/page.tsx
@@ -10,7 +10,6 @@ export default async function RedirectApp() {
     redirect('https://apps.apple.com/us/app/redi-love/id6754899018');
   }
   else {
-    console.log(process.env.NEXT_PUBLIC_BASE_URL)
     redirect(`${process.env.NEXT_PUBLIC_BASE_URL ?? 'https://redi.love'}/`);
   }
 
